9ad7aea7a0bfd49735196550f7abda4ddbd25bec
Priyanshu Sharma <priyanshu160101@gmail.com>
2023-06-10 13:12:10 +0530

    added db

---
+++ models/problem.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+// Define the problem schema
+const problemSchema = new mongoose.Schema({
+  
+  id: {
+        type: Number,
+        unique: true,
+        required: true
+  },
+  title: {
+    type: String,
+    required:true
+  },
+  description: {
+    type: String,
+    required:true
+  },
+  difficulty: {
+    type: String,
+    required:true
+  },
+  submissions: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      code: {
+        type: String,
+        required: true
+      },
+      language: {
+        type: String,
+        required: true
+      },
+      result: {
+        type: String,
+        enum: ['Accepted', 'Wrong Answer', 'Time Limit Exceeded', 'Runtime Error'],
+        default: 'Accepted'
+      }
+    }
+  ]
+});
+
+
+
+module.exports = mongoose.model('Problem', problemSchema);
---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+// Export the models
+module.exports = {
+  User
+};
