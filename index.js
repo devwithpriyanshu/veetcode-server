79a2ab0c8ace53fbec499dde16a40926e4f510a0
Priyanshu Sharma <priyanshu160101@gmail.com>
2023-07-30 17:58:38 +0530

    fixing mongo uri error

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(function (req, res, next) {
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
